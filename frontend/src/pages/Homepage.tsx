01fd4e8f55e13dae565b680db0bad5a37dde6369
fuaberu <kevinfabel@gmail.com>
2022-01-18 13:34:13 -0300

    load latest products

--- frontend/src/pages/Homepage.tsx
+++ frontend/src/pages/Homepage.tsx
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styled from 'styled-components';
+import { useGetAllProductsQuery } from '../app/sevices/products';
+import ProductCard from '../components/product/ProductCard';
 
 const Homepage = () => {
+	const { data, error, isLoading } = useGetAllProductsQuery();
+
+	useEffect(() => {
+		console.log(data, error);
+	}, [error, data]);
+
 	return (
 		<div>
 			<HeroContainer>hero</HeroContainer>
 			<ProductsContainer>
 				<h2>Latest Products</h2>
-				<div></div>
-				<div>products</div>
+				<ProductsDisplay>
+					{data &&
+						data.products.map((prod, index) => {
+							return <ProductCard key={index} product={prod} />;
+						})}
+				</ProductsDisplay>
 			</ProductsContainer>
 		</div>
 	);
@@ -20,6 +32,13 @@ const HeroContainer = styled.section`
 
 const ProductsContainer = styled.section`
 	min-height: 100vh;
+`;
+
+export const ProductsDisplay = styled.div`
+	display: grid;
+	grid-template-columns: repeat(4, 1fr);
+	gap: 1rem;
+	padding: 1rem;
 `;
 
 export default Homepage;
