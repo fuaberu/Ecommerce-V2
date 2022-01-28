b7dcf758a0f3f242ab19286599167c221380df13
fuaberu <kevinfabel@gmail.com>
2022-01-28 18:25:31 -0300

    add admin product action

--- frontend/src/app/sevices/products.ts
+++ frontend/src/app/sevices/products.ts
@@ -1,74 +1,136 @@
 import {
-	BaseQueryFn,
-	createApi,
-	FetchArgs,
-	fetchBaseQuery,
-} from '@reduxjs/toolkit/query/react';
+  BaseQueryFn,
+  createApi,
+  FetchArgs,
+  fetchBaseQuery,
+} from "@reduxjs/toolkit/query/react";
 
 export interface Product {
-	_id: string;
-	name: string;
-	description: string;
-	price: number;
-	stock: number;
-	numOfReviews: number;
-	reviews: {
-		id: string;
-		name: string;
-		user: string;
-		comment: string;
-		rating: number;
-	}[];
-	rating: number;
-	mainImage: {
-		public_id: string;
-		url: string;
-	};
-	detailedImages: {
-		public_id: string;
-		url: string;
-	}[];
+  _id: string;
+  name: string;
+  description: string;
+  price: number;
+  stock: number;
+  numOfReviews: number;
+  reviews: {
+    id: string;
+    name: string;
+    user: string;
+    comment: string;
+    rating: number;
+  }[];
+  rating: number;
+  mainImage: string;
+  detailedImages: string[];
+  category: string;
+  createdAt: Date;
+}
 
-	category: string;
-	createdAt: Date;
+export interface NewProduct {
+  name: string;
+  description: string;
+  price: number;
+  stock: number;
+  numOfReviews: number;
+  mainImage: string;
+  detailedImages: string[];
+  category: string;
 }
 
 interface ProductQueryResponse {
-	nextPage: number | null;
-	page: number;
-	products: Product[];
-	success: boolean;
-	highestPrice: number;
+  nextPage: number | null;
+  page: number;
+  products: Product[];
+  success: boolean;
+  highestPrice: number;
+  categories: string[];
 }
 interface ProductResponse {
-	product: Product;
-	success: boolean;
+  product: Product;
+  success: boolean;
+}
+interface CategoriesResponse {
+  categories: string[];
+  success: boolean;
 }
 interface CustomError {
-	status: number;
-	data: { message: string };
+  status: number;
+  data: { message: string };
 }
 
 // Define a service using a base URL and expected endpoints
 export const productsApi = createApi({
-	reducerPath: 'productsApi',
-	baseQuery: fetchBaseQuery({
-		baseUrl: 'http://localhost:4000/api/products',
-	}) as BaseQueryFn<string | FetchArgs, unknown, CustomError, {}>,
-	endpoints: (builder) => ({
-		getAllProducts: builder.query<ProductQueryResponse, void>({
-			query: () => '',
-		}),
-		getProductsList: builder.query<ProductQueryResponse, string>({
-			query: (query) => query,
-		}),
-		getAProduct: builder.query<ProductResponse, string>({
-			query: (id) => `${id}`,
-		}),
-	}),
+  reducerPath: "productsApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "http://localhost:4000/api/products",
+  }) as BaseQueryFn<string | CustomError | FetchArgs, unknown, CustomError, {}>,
+  endpoints: (builder) => ({
+    //all
+    getAllProducts: builder.query<ProductQueryResponse, void>({
+      query: () => "/",
+    }),
+    getProductsList: builder.query<ProductQueryResponse, string>({
+      query: (query) => query,
+    }),
+    getAProduct: builder.query<ProductResponse, string>({
+      query: (id) => `${id}`,
+    }),
+    //user
+    getProductsCategories: builder.query<CategoriesResponse, void>({
+      query: () => ({
+        url: `/categories`,
+        method: "GET",
+        credentials: "include",
+      }),
+    }),
+    setProductStock: builder.mutation<
+      ProductResponse,
+      { productId: string; stock: number }
+    >({
+      query: ({ productId, stock }) => ({
+        url: `/${productId}`,
+        method: "PUT",
+        stock,
+        credentials: "include",
+      }),
+    }),
+    //admin
+    updateProduct: builder.mutation<
+      ProductResponse,
+      { productId: string; body: any }
+    >({
+      query: ({ productId, body }) => ({
+        url: `/${productId}`,
+        method: "PUT",
+        body,
+        credentials: "include",
+      }),
+    }),
+    createProduct: builder.mutation<ProductResponse, NewProduct>({
+      query: (body) => ({
+        url: `/admin/new`,
+        method: "POST",
+        body,
+        credentials: "include",
+      }),
+    }),
+    deleteProduct: builder.mutation<ProductResponse, string>({
+      query: (productId) => ({
+        url: `/admin/${productId}`,
+        method: "DELETE",
+        credentials: "include",
+      }),
+    }),
+  }),
 });
 
 // Export hooks for usage in functional components, which are
 // auto-generated based on the defined endpoints
-export const { useGetProductsListQuery, useGetAllProductsQuery, useGetAProductQuery } =
-	productsApi;
+export const {
+  useGetProductsListQuery,
+  useGetAllProductsQuery,
+  useGetAProductQuery,
+  useCreateProductMutation,
+  useDeleteProductMutation,
+  useGetProductsCategoriesQuery,
+} = productsApi;
--- frontend/src/pages/dashboard/ProductsBoard.tsx
+++ frontend/src/pages/dashboard/ProductsBoard.tsx
@@ -1,32 +1,57 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
-import { useGetAllOrdersQuery } from "../../app/sevices/orders";
-import { useGetAllProductsQuery } from "../../app/sevices/products";
+import {
+  useDeleteProductMutation,
+  useGetAllProductsQuery,
+} from "../../app/sevices/products";
 import ProductTabelHead from "../../components/product/ProductTabelHead";
 import { BsTrash } from "react-icons/bs";
 import { AiOutlineEdit } from "react-icons/ai";
-
 const ProductsBoard = () => {
-  const { data, error, isLoading } = useGetAllProductsQuery();
+  const [deleting, setDeleting] = useState(false);
+
+  const { data, error, isLoading, refetch } = useGetAllProductsQuery();
+
+  const [deleteProduct, { isLoading: isDeleting, error: deleteError }] =
+    useDeleteProductMutation();
+
+  useEffect(() => {
+    if (isDeleting || isLoading) {
+      setDeleting(true);
+    } else {
+      setDeleting(false);
+    }
+  }, [isDeleting, isLoading]);
+
+  const deleteData = (id: string) => {
+    //delete from db and then refetch the data
+    deleteProduct(id).then((e) => {
+      console.log(e);
+      refetch();
+    });
+  };
   return (
     <div>
       <ProductTabelHead admin />
-      {data?.products.map((product, index) => (
-        <ProductContainer>
-          <p style={{ flex: 1.5 }}>{product._id}</p>
-          <p>{product.name}</p>
-          <p>{product.stock}</p>
-          <p>{product.price}</p>
-          <div>
-            <button>
-              <BsTrash size={24} />
-            </button>
-            <button>
-              <AiOutlineEdit size={24} />
-            </button>
-          </div>
-        </ProductContainer>
-      ))}
+      {!error
+        ? data?.products.map((product, index) => (
+            <ProductContainer key={index}>
+              <p style={{ flex: 1.5 }}>{product._id}</p>
+              <p>{product.name}</p>
+              <p>{product.stock}</p>
+              <p>{product.price}</p>
+              <div>
+                <button onClick={() => deleteData(product._id)}>
+                  <BsTrash size={24} />
+                </button>
+                <button>
+                  <AiOutlineEdit size={24} />
+                </button>
+              </div>
+            </ProductContainer>
+          ))
+        : console.log(error, data)}
+      {deleting && <h1>deleting</h1>}
     </div>
   );
 };
