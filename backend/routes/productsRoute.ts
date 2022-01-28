f139debb792873e19d671cdc39db8364460bc8b9
fuaberu <kevinfabel@gmail.com>
2022-01-28 14:42:38 -0300

    change product update access

--- backend/routes/productsRoute.ts
+++ backend/routes/productsRoute.ts
@@ -1,44 +1,39 @@
-import { Router } from 'express';
+import { Router } from "express";
 import {
-	createProduct,
-	createReview,
-	deleteProduct,
-	deleteReview,
-	getAllProducts,
-	getProduct,
-	udpateProduct,
-} from '../controllers/productController';
-import { authorizedRoles, isAuthenticated } from '../utils/isAuth';
+  createProduct,
+  createReview,
+  deleteProduct,
+  deleteReview,
+  getAllProducts,
+  getProduct,
+  udpateProduct,
+} from "../controllers/productController";
+import { authorizedRoles, isAuthenticated } from "../utils/isAuth";
 
 const ProductsRoutes = Router();
 
 //ADMIN AREA
 ProductsRoutes.post(
-	'/admin/new',
-	isAuthenticated,
-	authorizedRoles('admin'),
-	createProduct
+  "/admin/new",
+  isAuthenticated,
+  authorizedRoles("admin"),
+  createProduct
 );
 
-ProductsRoutes.put(
-	'/admin/:productId',
-	isAuthenticated,
-	authorizedRoles('admin'),
-	udpateProduct
-);
 ProductsRoutes.delete(
-	'/admin/:productId',
-	isAuthenticated,
-	authorizedRoles('admin'),
-	deleteProduct
+  "/admin/:productId",
+  isAuthenticated,
+  authorizedRoles("admin"),
+  deleteProduct
 );
 
 //AUTHENTICATED AREA
-ProductsRoutes.post('/review', isAuthenticated, createReview);
-ProductsRoutes.delete('/review', isAuthenticated, deleteReview);
+ProductsRoutes.put("/:productId", isAuthenticated, udpateProduct);
+ProductsRoutes.post("/review", isAuthenticated, createReview);
+ProductsRoutes.delete("/review", isAuthenticated, deleteReview);
 
 //ALL USERS AREA
-ProductsRoutes.get('/', getAllProducts);
-ProductsRoutes.get('/:productId', getProduct);
+ProductsRoutes.get("/", getAllProducts);
+ProductsRoutes.get("/:productId", getProduct);
 
 export default ProductsRoutes;
